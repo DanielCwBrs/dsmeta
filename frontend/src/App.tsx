9ab4dbef0c180693c0b6a34e9f873140998b63e1
Daniel Amaral Pereira <dap93534@gmail.com>
2022-09-07 12:02:16 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationButton from "./components/NotificationButton"
-import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
 
   )
